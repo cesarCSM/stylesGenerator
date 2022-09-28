183dce8676ae6c8e08dfbaaa13ce401c7d36da6f
Cesar <cesar@rebus.foundation>
2022-09-28 11:07:47 -0400

    bugFixingOnContentStylesEditor

--- globalVars/colorVars.js
+++ globalVars/colorVars.js
@@ -1,5 +1,4 @@
 const colorCompStyles = [
   '<link rel="stylesheet" href="../styles/global.css" />',
   '<link rel="stylesheet" href="../styles/pageButton.css" />',
-  '<link rel="stylesheet" href="../styles/pageCheckbox.css" />',
 ];
